e08821d3d2dc5be266c61bd7cf9e43f7529a1a25
DiaraMar <diarranice@gmail.com>
2022-07-12 16:09:27 +0200

    first commit

---
+++ src/testDockerII/Application.java
@@ -0,0 +1,14 @@
+package testDockerII;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("test Docker");
+		for (int i =0; i<4; i++) {
+			System.out.println("Line  " + (i+1));
+			
+		}
+
+	}
+
+}
